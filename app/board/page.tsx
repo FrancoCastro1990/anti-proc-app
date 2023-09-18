9927dbe1686b819f7c4f36d23377328378f6ff10
Franco Castro V <franco.castro.villanueva.90@gmail.com>
2023-09-18 00:57:58 -0300

    implement drag and drop

--- app/board/page.tsx
+++ app/board/page.tsx
@@ -42,27 +42,56 @@ const Board = () => {
     setCards(newCards);
   };
 
+  const moveCard = (id: number, section: string) => {
+    const newCards = cards.map((card) => {
+      if (card.id === id) {
+        return { ...card, section };
+      }
+      return card;
+    });
+    setCards(newCards);
+  };
+
   return (
-    <main className="flex min-h-screen bg-slate-800 flex-col p-4">
+    <main className="flex min-h-screen bg-slate-800 flex-col p-4 overflow-hidden">
       <h1 className="text-center">Board</h1>
-      <div className="flex flex-row justify-around min-w-full mt-1">
-        <section className="min-h-fit w-1/3 flex flex-col gap-4">
+      <div className="flex flex-row gap-1 min-w-full mt-1 min-h-full">
+        <section
+          className="min-h-screen w-80 flex flex-col gap-4 box-content border-2 border-gray-600 rounded-md p-2 "
+          onDragEnter={(e) => {
+            console.log("drag enter in backlog", e);
+            e.stopPropagation();
+            e.currentTarget.classList.add("border-dotted");
+          }}
+          onDragLeave={(e) => {
+            console.log("drag leave in backlog", e);
+            e.stopPropagation();
+            e.currentTarget.classList.remove("border-dotted");
+          }}
+          onDragOver={(e) => {
+            e.preventDefault();
+            return false;
+          }}
+          onDrop={(e) => {
+            e.currentTarget.classList.remove("border-dotted");
+            e.stopPropagation();
+            const cardId = e.dataTransfer.getData("cardId");
+            e.currentTarget.classList.remove("opacity-50");
+            e.dataTransfer.clearData();
+            const card = cards.find((card) => card.id === Number(cardId));
+            if (card) {
+              moveCard(card.id, "backlog");
+            }
+            const dragImages = document.querySelectorAll("#drag-image");
+            dragImages.forEach((image) => {
+              image.innerHTML = "";
+            });
+            return false;
+          }}
+        >
           <h2 className="text-center pb-2">backlog</h2>
-          {cards.map((card) => {
-            return (
-              card.section === "backlog" && (
-                <Card
-                  key={card.id}
-                  id={card.id}
-                  removeCard={removeCard}
-                  title={card.title}
-                  updateCard={updateCard}
-                />
-              )
-            );
-          })}
           <button
-            className="bg-slate-600 shadow-lg rounded-full overflow-hidden p-2 self-end"
+            className="transition-all ease-in-out duration-300 bg-slate-600 shadow-lg rounded-full overflow-hidden p-2 self-end hover:transform hover:scale-110 "
             onClick={addCard}
           >
             <svg
@@ -76,8 +105,50 @@ const Board = () => {
               <path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32v144H48c-17.7 0-32 14.3-32 32s14.3 32 32 32h144v144c0 17.7 14.3 32 32 32s32-14.3 32-32V288h144c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
             </svg>
           </button>
+          {cards.map((card) => {
+            return (
+              card.section === "backlog" && (
+                <Card
+                  key={card.id}
+                  id={card.id}
+                  removeCard={removeCard}
+                  title={card.title}
+                  updateCard={updateCard}
+                />
+              )
+            );
+          })}
         </section>
-        <section className="min-h-fit w-1/3 flex flex-col gap-1">
+        <section
+          className="min-h-screen w-80 flex flex-col gap-4 box-content border-2 border-gray-600 rounded-md p-2"
+          onDragEnter={(e) => {
+            e.currentTarget.classList.add("border-dotted");
+          }}
+          onDragLeave={(e) => {
+            e.currentTarget.classList.remove("border-dotted");
+          }}
+          onDragOver={(e) => {
+            e.preventDefault();
+            return false;
+          }}
+          onDrop={(e) => {
+            e.currentTarget.classList.remove("border-dotted");
+
+            e.stopPropagation();
+            const cardId = e.dataTransfer.getData("cardId");
+            e.currentTarget.classList.remove("opacity-50");
+            e.dataTransfer.clearData();
+            const card = cards.find((card) => card.id === Number(cardId));
+            if (card) {
+              moveCard(card.id, "in-progress");
+            }
+            const dragImages = document.querySelectorAll("#drag-image");
+            dragImages.forEach((image) => {
+              image.innerHTML = "";
+            });
+            return false;
+          }}
+        >
           <h2 className="text-center pb-2">in progress</h2>
           {cards.map((card) => {
             return (
@@ -93,7 +164,37 @@ const Board = () => {
             );
           })}
         </section>
-        <section className="min-h-fit w-1/3 flex flex-col">
+        <section
+          className="min-h-screen w-80 flex flex-col gap-4 box-content border-2 border-gray-600 rounded-md p-2"
+          onDragEnter={(e) => {
+            e.currentTarget.classList.add("border-dotted");
+          }}
+          onDragLeave={(e) => {
+            e.currentTarget.classList.remove("border-dotted");
+          }}
+          onDragOver={(e) => {
+            e.preventDefault();
+            return false;
+          }}
+          onDrop={(e) => {
+            e.currentTarget.classList.remove("border-dotted");
+
+            e.stopPropagation();
+            const cardId = e.dataTransfer.getData("cardId");
+            e.currentTarget.classList.remove("opacity-50");
+            e.dataTransfer.clearData();
+
+            const card = cards.find((card) => card.id === Number(cardId));
+            if (card) {
+              moveCard(card.id, "ready");
+            }
+            const dragImages = document.querySelectorAll("#drag-image");
+            dragImages.forEach((image) => {
+              image.innerHTML = "";
+            });
+            return false;
+          }}
+        >
           <h2 className="text-center pb-2">ready</h2>
           {cards.map((card) => {
             return (
@@ -110,6 +211,7 @@ const Board = () => {
           })}
         </section>
       </div>
+      <div id="drag-image"></div>
     </main>
   );
 };
--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -13,6 +13,7 @@ const Card = ({ title, section, id, removeCard, updateCard }: Prop) => {
   const [cardTitle, setCardTitle] = React.useState(title);
 
   const inputRef = useRef<HTMLInputElement>(null);
+  const cardRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     if (isEditing && inputRef.current) {
@@ -20,12 +21,53 @@ const Card = ({ title, section, id, removeCard, updateCard }: Prop) => {
       inputRef.current.select();
     }
   }, [isEditing]);
+
+  const onDragStart = (e: React.DragEvent<HTMLDivElement>) => {
+    console.log("drag start", e);
+    if (!cardRef.current) return;
+    cardRef.current.classList.add("opacity-70");
+    e.dataTransfer.effectAllowed = "move";
+
+    const width = cardRef.current.offsetWidth - 24;
+
+    var crt = document.getElementById("drag-image") as HTMLDivElement;
+    //crt.style.transform = "scale(0.5)";
+    crt.style.opacity = "1";
+    crt.style.position = "absolute";
+    crt.style.top = "0px";
+    crt.style.left = "-" + cardRef.current.offsetWidth + "px";
+    crt.style.width = width + "px";
+    crt.style.height = 84 + "px";
+    crt.classList.add(
+      "bg-slate-600",
+      "rounded-lg",
+      "overflow-hidden",
+      "p-2",
+      "border-2",
+      "border-slate-500"
+    );
+    crt.innerHTML = cardTitle;
+    //e.dataTransfer.setDragImage(cardRef.current, cardRef.current.offsetWidth - 80, 60);
+    e.dataTransfer.setDragImage(crt, cardRef.current.offsetWidth - 80, 60);
+
+    e.dataTransfer.setData("cardId", id.toString());
+  };
+
+  const onDragEnd = (e: React.DragEvent<HTMLDivElement>) => {
+    if (!cardRef.current) return;
+    cardRef.current.classList.remove("opacity-70");
+  };
+
   return (
-    <div className="bg-slate-600 shadow-lg rounded-lg overflow-hidden p-2 flex flex-col">
+    <div
+      ref={cardRef}
+      className="box-border border-2 border-slate-600 bg-slate-600 shadow-lg rounded-lg overflow-hidden p-2 flex flex-col transition-all ease-in-out duration-300 hover:border-2 hover:border-slate-500"
+    >
       <div
         onDoubleClick={() => {
           setIsEditing(!isEditing);
         }}
+        className="cursor-text"
       >
         {isEditing ? (
           <input
@@ -48,28 +90,48 @@ const Card = ({ title, section, id, removeCard, updateCard }: Prop) => {
             }}
           />
         ) : (
-          <h3 className="p-1 overflow-hidden text-ellipsis whitespace-nowrap w-full">
+          <h3 className="transition ease-in-out p-1 overflow-hidden text-ellipsis whitespace-nowrap w-full select-none hover:bg-slate-500 rounded-md ">
             {title}
           </h3>
         )}
       </div>
 
-      <button
-        onClick={() => removeCard(id)}
-        className="bg-slate-700 shadow-lg rounded-full overflow-hidden p-2 mt-1 w-8 self-end"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          height="1em"
-          style={{
-            fill: "red",
-            margin: "auto",
-          }}
-          viewBox="0 0 448 512"
+      <div className="flex gap-1 self-end ">
+        <div
+          className="transition-all ease-in-out duration-300 bg-slate-700 shadow-lg rounded-full overflow-hidden p-2 mt-1 w-8 cursor-move hover:transform hover:scale-110"
+          draggable={true}
+          onDragStart={onDragStart}
+          onDragEndCapture={onDragEnd}
         >
-          <path d="M135.2 17.7 128 32H32C14.3 32 0 46.3 0 64s14.3 32 32 32h384c17.7 0 32-14.3 32-32s-14.3-32-32-32h-96l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32l21.2 339c1.6 25.3 22.6 45 47.9 45h245.8c25.3 0 46.3-19.7 47.9-45L416 128z" />
-        </svg>
-      </button>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="1em"
+            style={{
+              fill: "#ffffff",
+            }}
+            viewBox="0 0 512 512"
+          >
+            <path d="M278.6 9.4c-12.5-12.5-32.8-12.5-45.3 0l-64 64c-9.2 9.2-11.9 22.9-6.9 34.9s16.6 19.8 29.6 19.8h32v96h-96V192c0-12.9-7.8-24.6-19.8-29.6s-25.7-2.2-34.9 6.9l-64 64c-12.5 12.5-12.5 32.8 0 45.3l64 64c9.2 9.2 22.9 11.9 34.9 6.9s19.8-16.6 19.8-29.6V288h96v96h-32c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l64 64c12.5 12.5 32.8 12.5 45.3 0l64-64c9.2-9.2 11.9-22.9 6.9-34.9S332.9 384 319.9 384H288v-96h96v32c0 12.9 7.8 24.6 19.8 29.6s25.7 2.2 34.9-6.9l64-64c12.5-12.5 12.5-32.8 0-45.3l-64-64c-9.2-9.2-22.9-11.9-34.9-6.9S384 179.1 384 192.1v32h-96V128h32c12.9 0 24.6-7.8 29.6-19.8s2.2-25.7-6.9-34.9l-64-64z" />
+          </svg>
+        </div>
+
+        <button
+          onClick={() => removeCard(id)}
+          className="transition-all ease-in-out duration-300 bg-slate-700 shadow-lg rounded-full overflow-hidden p-2 mt-1 w-8 hover:transform hover:scale-110 "
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="1em"
+            style={{
+              fill: "red",
+              margin: "auto",
+            }}
+            viewBox="0 0 448 512"
+          >
+            <path d="M135.2 17.7 128 32H32C14.3 32 0 46.3 0 64s14.3 32 32 32h384c17.7 0 32-14.3 32-32s-14.3-32-32-32h-96l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32l21.2 339c1.6 25.3 22.6 45 47.9 45h245.8c25.3 0 46.3-19.7 47.9-45L416 128z" />
+          </svg>
+        </button>
+      </div>
     </div>
   );
 };
